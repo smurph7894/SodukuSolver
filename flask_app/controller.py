673bfd65aea1673e6d6117ebed65b6e85e9290e8
smurph7894 <104289709+smurph7894@users.noreply.github.com>
2022-11-23 11:22:42 -0800

    add input function, start solver, basic html

--- flask_app/controller.py
+++ flask_app/controller.py
@@ -1,3 +1,18 @@
 from flask_app import app
-from flask import render_tempalte, redirect, request
+from flask import render_template, redirect, request
 from flask_app import solver 
+
+
+@app.route('/new-sudoku')
+def home():
+        return render_template('newSudoku.html')
+
+@app.route ('/solve-sudoku', methods=['POST'])
+def new_sudoku():
+        if solver.solve_one(request.form):
+                return redirect('/results')
+
+@app.route('/results')
+def solved_sudoku():
+        results = solver.solve_one
+        return render_template('results.html', results=results)
--- flask_app/solver.py
+++ flask_app/solver.py
@@ -1,7 +1,66 @@
 from flask_app import app
 
-class Solver:
+DEBUG = True
+#set DEBUG to TRUE when you want prints to log
 
-    def __init__(self, data):
-        self.horizontalLine = data['horizontalLine'],
-        self.verticalLine = data['verticalLine']
+def log(inputValue):
+    if DEBUG : 
+        print(inputValue)
+
+
+
+def take_input (puzzle):
+    log(puzzle)
+    log(puzzle["0-1"])
+    sudokuData=[[],[],[],[],[],[],[],[],[]]
+    i=0
+    while (i<=8):
+        j=0
+        while(j<=8):
+            sudokuData[i].append(puzzle[f"{i}-{j}"])
+            j+=1
+        i+=1
+    log(sudokuData)
+    return sudokuData  
+
+def get_row(data, rowIndex):
+    oneRow = []
+    i=rowIndex
+    while(i<=rowIndex):
+        j=0
+        while(j<=8):
+            oneRow.append(data[i-j])
+        j+=1
+    return oneRow
+
+def get_column(data, columnIndex):
+    oneColumn = []
+    i=0
+    while(i<=8):
+        j=columnIndex
+        while(j<=columnIndex):
+            oneColumn.append(data[i-j])
+            j+=1
+    return oneColumn
+
+def get_box(data, columnIndex, rowIndex):
+    oneBox = []
+    i=rowIndex
+    while(i<=rowIndex+3):
+        j=columnIndex
+        while(j<=columnIndex+3):
+            oneBox.append(data[i-j])
+            j+=1
+    return oneBox
+
+def solve_one(data):
+    dataSet = take_input(data)
+    row = get_row(dataSet, 0)
+    log(row)
+    column = get_column(dataSet,0)
+    log(column)
+    box = get_box(dataSet, 0, 0)
+    log(box)
+    return True
+# compare all vertical to horizontal for no repeating numbers
+# maybe sort
